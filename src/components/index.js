9e3436a0d4c06c607de9ea7613ab29df9108339c
javier-tralma <javier.tralma@sansano.usm.cl>
2023-08-03 18:15:18 -0400

    feat: update components in index

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,17 @@
 import Navbar from './Navbar';
 import Hero from './Hero';
 import AboutUs from './AboutUs';
+import Collaborators from './Collaborators';
+import Team from './Team';
+import ContactUs from './ContactUs';
+import Modal from './Modal';
 
 export {
     Navbar,
     Hero,
     AboutUs,
+    Collaborators,
+    Team,
+    ContactUs,
+    Modal,
 };
